53c6eb2a920c72cff260c6c0205910837d6971e4
Yef1244 <yeferson1244@gmail.com>
2021-09-10 07:41:20 -0500

    Se crean puntos del taller2

---
+++ punto1.js
@@ -0,0 +1,16 @@
+let padawan={
+    nombre:"juan",
+    planeta:"yavin4",
+    edad:"14",
+    estatura:"1.70"
+}
+
+function matricularPadawan(padawan.edad){
+    if(edad<15 && edad>=15){
+        let mensaje="El padawan fue asigando a Manejo de fuerza"
+        console.log(mensaje)
+    }else{
+        let mensaje="El padawan fue asignado a Sable de luz"
+        console.log(mensaje)
+    }
+}
